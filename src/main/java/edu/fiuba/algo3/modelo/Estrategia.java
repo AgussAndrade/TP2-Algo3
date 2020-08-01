80c3b449951d8f22905f05f2072e84c43e04d8f8
AgussAndrade <51175036+AgussAndrade@users.noreply.github.com>
2020-07-31 22:05:36 -0300

    Arreglo estrategia

--- src/main/java/edu/fiuba/algo3/modelo/Estrategia.java
+++ src/main/java/edu/fiuba/algo3/modelo/Estrategia.java
@@ -1,8 +1,10 @@
 package edu.fiuba.algo3.modelo;
 
+import javafx.scene.control.skin.SliderSkin;
+
 import java.util.List;
 
 public interface Estrategia {
-    void sumarPuntos(Respuesta respuesta);
+    void sumarPuntos(Respuesta respuesta , List<Opcion> opciones);
 
 }
