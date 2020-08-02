70829a323045b0fc9726d1da24360a5ee845aa0b
NCellini <66921064+NCellini@users.noreply.github.com>
2020-08-01 23:27:07 -0300

    Se agregaron pruebas de la clase OrderedChoice

---
+++ src/main/test/edu/fiuba/algo3/modelo/OrderedChoiceTest.java
@@ -0,0 +1,72 @@
+package edu.fiuba.algo3.modelo;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class OrderedChoiceTest {
+    @Test
+    public void test01UnaPreguntaOrderedChoiceCompruebaUnaRespuestaCorrectaYSumaPuntos() {
+        String nombre = "Ordenar Alfabeticamente las siguientes provincias";
+
+        List<Opcion> opciones = new ArrayList<>();
+        opciones.add(new OpcionPosicion("Buenos Aires", 1));
+        opciones.add(new OpcionPosicion("La pampa",2));
+        opciones.add(new OpcionPosicion("Salta",3));
+        opciones.add(new OpcionPosicion("Tierra del Fuego",4));
+        opciones.add(new OpcionPosicion("Tucuman",5));
+
+        Pregunta pregunta = new OrderedChoice(nombre, opciones, new Clasica());
+
+        Jugador jugador = new Jugador("Juan");
+
+        opciones.get(0).seleccionar(1);
+        opciones.get(1).seleccionar(2);
+        opciones.get(2).seleccionar(3);
+        opciones.get(3).seleccionar(4);
+        opciones.get(4).seleccionar(5);
+
+
+        List<Respuesta> respuestas = new ArrayList<>();
+
+        respuestas.add(new Respuesta(jugador, opciones));
+
+        pregunta.comprobarRespuestas(respuestas);
+
+        assertEquals(1, jugador.puntos());
+    }
+
+    @Test
+    public void test02UnaPreguntaOrderedChoiceCompruebaUnaRespuestaIncorrectaYNoSumaPuntos() {
+        String nombre = "Ordenar Alfabeticamente las siguientes provincias";
+
+        List<Opcion> opciones = new ArrayList<>();
+        opciones.add(new OpcionPosicion("Buenos Aires", 1));
+        opciones.add(new OpcionPosicion("La pampa",2));
+        opciones.add(new OpcionPosicion("Salta",3));
+        opciones.add(new OpcionPosicion("Tierra del Fuego",4));
+        opciones.add(new OpcionPosicion("Tucuman",5));
+
+        Pregunta pregunta = new OrderedChoice(nombre, opciones, new Clasica());
+
+        Jugador jugador = new Jugador("Juan");
+
+
+        opciones.get(0).seleccionar(5);
+        opciones.get(1).seleccionar(3);
+        opciones.get(2).seleccionar(2);
+        opciones.get(3).seleccionar(1);
+        opciones.get(4).seleccionar(4);
+
+
+        List<Respuesta> respuestas = new ArrayList<>();
+
+        respuestas.add(new Respuesta(jugador, opciones));
+
+        pregunta.comprobarRespuestas(respuestas);
+
+        assertEquals(0, jugador.puntos());
+    }
+}
