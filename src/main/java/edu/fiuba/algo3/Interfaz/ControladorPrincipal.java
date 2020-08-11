2a654621e4b8230a6b90f3112ba0afda26b83091
AgussAndrade <51175036+AgussAndrade@users.noreply.github.com>
2020-08-11 01:21:25 -0300

    cambio variables

--- src/main/java/edu/fiuba/algo3/Interfaz/ControladorPrincipal.java
+++ src/main/java/edu/fiuba/algo3/Interfaz/ControladorPrincipal.java
@@ -1,16 +1,17 @@
 package edu.fiuba.algo3.Interfaz;
 
 import edu.fiuba.algo3.modelo.Jugador;
+import edu.fiuba.algo3.modelo.lectorDePreguntas.ColeccionDePreguntas;
 import edu.fiuba.algo3.modelo.preguntas.Pregunta;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class ControladorPrincipal {
-    static public List<Pregunta> preguntas;
+    static public ColeccionDePreguntas preguntas;
     static public List<Jugador> jugadores;
 
-    public static void inicializar(List<Pregunta> _preguntas){
+    public static void inicializar(ColeccionDePreguntas _preguntas){
         jugadores = new ArrayList<>();
         preguntas = _preguntas;
     }
