d4c04a28d24d82d116cb0b23707c80a01dbb9665
AgussAndrade <51175036+AgussAndrade@users.noreply.github.com>
2020-08-16 00:11:02 -0300

    Elimino kahoot

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -1,15 +1,20 @@
 package edu.fiuba.algo3;
 
+import edu.fiuba.algo3.controlador.ControladorPrincipal;
+import edu.fiuba.algo3.lectorDePreguntas.LectorDePreguntasJson;
+import edu.fiuba.algo3.modelo.preguntas.Pregunta;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import java.util.List;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Kahoot kahoot = new Kahoot();
-        kahoot.cargarPreguntas("preguntas.json");
-        kahoot.iniciarVista(stage);
+        LectorDePreguntasJson lectorDePreguntas = new LectorDePreguntasJson();
+        List<Pregunta> preguntas = lectorDePreguntas.leerPreguntas("preguntas.json");
+        ControladorPrincipal.inicializar(preguntas, stage);
     }
 
     public static void main(String[] args) {
