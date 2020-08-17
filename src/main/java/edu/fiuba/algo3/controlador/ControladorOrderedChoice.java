afe2ab764a9b60d1b025af40d20040b9b9569ec0
AgussAndrade <51175036+AgussAndrade@users.noreply.github.com>
2020-08-16 22:25:04 -0300

    Agrego funcion de multiplicadores a ordered choice

--- src/main/java/edu/fiuba/algo3/controlador/ControladorOrderedChoice.java
+++ src/main/java/edu/fiuba/algo3/controlador/ControladorOrderedChoice.java
@@ -1,5 +1,6 @@
 package edu.fiuba.algo3.controlador;
 
+import edu.fiuba.algo3.modelo.multiplicadores.Multiplicador;
 import edu.fiuba.algo3.modelo.opciones.Posicionable;
 import edu.fiuba.algo3.modelo.preguntas.OrderedChoice;
 import javafx.event.ActionEvent;
@@ -37,5 +38,12 @@ public class ControladorOrderedChoice extends ControladorPregunta{
     }
 
     public void activarExclusividad(ActionEvent actionEvent) {
+    }
+    public void activarMultiplicadorX2(ActionEvent actionEvent) {
+       // constructorDeRespuestaActual.conMultiplicador(new Multiplicador(2));
+    }
+
+    public void activarMultiplicadorX3(ActionEvent actionEvent) {
+     //   constructorDeRespuestaActual.conMultiplicador(new Multiplicador(3));
     }
 }
