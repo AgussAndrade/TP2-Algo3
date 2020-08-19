dfebf929570dee80f08779c74050fb9bcb98e673
Dimartino <pdimartino@fi.uba.ar>
2020-08-19 14:24:35 -0300

    Arreglo de la prueba LectorDePreguntasTest

--- src/main/test/edu/fiuba/algo3/modelo/LectorDePreguntasTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/LectorDePreguntasTest.java
@@ -13,15 +13,27 @@ public class LectorDePreguntasTest {
     public void test01LectorDePreguntasLeeCorrectamenteUnArchivoEspecificado(){
         LectorDePreguntasJson lector = new LectorDePreguntasJson();
         List<String> enunciados = List.of(
-                                        "Argentina es un país", "Ordenar de menor a mayor poblacion de paises", "¿Argentina es una construccion social?",
-                                        "¿Cual de los siguientes jugadores de futbol tiene mas asistencias?", "Separar paises de ciudades",
-                                        "¿Cual de los siguientes nombres son capicua?", "¿Cual de les siguientes pibis juega fortnite?",
-                                        "La capital de Austria es Amsterdam", "Ordenar de mayor a menor las superficies de los paises",
-                                        "Un pulpo tiene 5 corazones", "¿Quien pinto el cuadro Las meninas?","Separar futbolistas de basquetbolistas",
-                                        "¿Que ciudades pertenecen a Francia?", "¿Cual de estos sabores son primarios?", "Stephen Hawking rechazó el título de caballero de la Reina",
-                                        "Ordenar de menor a mayor las superficies de los países", "La estatua de la libertad fue un regalo de Francia",
-                                        "¿Cual de las siguientes personas ganaron un Nobel?", "Separar lenguajes de tipado estatico y dinamico",
-                                        "¿En que continente se encuentra el pais Eritrea?", "¿Cual de estos son lenguajes compilados?" );
+                                        "Separar futbolistas de basquetbolistas",
+                                        "Argentina es un país",
+                                        "Ordenar de menor a mayor poblacion de paises",
+                                        "¿Argentina es una construccion social?",
+                                        "¿Cual de los siguientes jugadores de futbol tiene mas asistencias?",
+                                        "Separar paises de ciudades",
+                                        "¿Cual de los siguientes nombres son capicua?",
+                                        "¿Cual de les siguientes pibis juega fortnite?",
+                                        "La capital de Austria es Amsterdam",
+                                        "Ordenar de mayor a menor las superficies de los paises",
+                                        "Un pulpo tiene 5 corazones",
+                                        "¿Quien pinto el cuadro Las meninas?",
+                                        "¿Que ciudades pertenecen a Francia?",
+                                        "¿Cual de estos sabores son primarios?",
+                                        "Stephen Hawking rechazó el título de caballero de la Reina",
+                                        "Ordenar de menor a mayor las superficies de los países",
+                                        "La estatua de la libertad fue un regalo de Francia",
+                                        "¿Cual de las siguientes personas ganaron un Nobel?",
+                                        "Separar lenguajes de tipado estatico y dinamico",
+                                        "¿En que continente se encuentra el pais Eritrea?",
+                                        "¿Cual de estos son lenguajes compilados?" );
         List<Pregunta> preguntas = lector.leerPreguntas("preguntas.json");
         int contador = 0;
         boolean elTestPasa = preguntas.size() == enunciados.size();
