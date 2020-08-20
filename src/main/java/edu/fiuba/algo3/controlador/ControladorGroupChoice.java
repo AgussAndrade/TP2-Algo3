e0e734f5f7ef41ee7775246ee4dd41b69c562456
Dimartino <pdimartino@fi.uba.ar>
2020-08-20 01:37:36 -0300

    Se mejoro el algoritmo de ControladorGroupChoice

--- src/main/java/edu/fiuba/algo3/controlador/ControladorGroupChoice.java
+++ src/main/java/edu/fiuba/algo3/controlador/ControladorGroupChoice.java
@@ -9,54 +9,60 @@ import javafx.scene.input.*;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Hashtable;
 import java.util.List;
 
 public class ControladorGroupChoice extends ControladorPregunta{
-    public Label nombreGrupoA;
-    public Label nombreGrupoB;
+    public Label nombreGrupoA, nombreGrupoB;
     public Label opcion1,opcion2,opcion3,opcion4,opcion5,opcion6;
     public VBox BoxGrupoB,BoxGrupoA,BoxSinGrupo;
-    private List<Grupal> selecciones;
-    public Label[] opciones;
-    public Label[] grupos;
-    public VBox[] boxGrupos;
-    public Hashtable<String,Label> mapaDeOpciones = new Hashtable<>();
+    private List<Grupal> opciones;
+    public List<Label> labelsOpciones = new ArrayList<>();
+    public List<Label> labelsGrupos = new ArrayList<>();
+    public List<VBox> boxGrupos = new ArrayList<>();
     public Hashtable<String,VBox> mapaDeGrupos = new Hashtable<>();
 
     public void initialize() {
         cargarBotones();
         inicializarLabelsPregunta();
         ajustarLabel(enunciadoPregunta);
-        selecciones = ((GroupChoice)preguntaActual).obtenerOpciones();
-        Collections.shuffle(selecciones);
+        opciones = ((GroupChoice)preguntaActual).obtenerOpciones();
+        Collections.shuffle(opciones);
         constructorDeRespuestaActual.conResponsable(jugadores.get(jugadorActual));
-        opciones = new Label[]{opcion1, opcion2, opcion3, opcion4, opcion5, opcion6};
-        boxGrupos = new VBox[]{BoxSinGrupo,BoxGrupoA,BoxGrupoB};
-        grupos = new Label[]{nombreGrupoA,nombreGrupoB};
+        labelsGrupos.add(nombreGrupoA);
+        labelsGrupos.add(nombreGrupoB);
+        labelsOpciones.add(opcion1);
+        labelsOpciones.add(opcion2);
+        labelsOpciones.add(opcion3);
+        labelsOpciones.add(opcion4);
+        labelsOpciones.add(opcion5);
+        labelsOpciones.add(opcion6);
+        boxGrupos.add(BoxSinGrupo);
+        boxGrupos.add(BoxGrupoA);
+        boxGrupos.add(BoxGrupoB);
         int i=0,j=0;
-        mapaDeGrupos.put("sinGrupo", boxGrupos[j++]);
-        boxGrupos[j].setId("sinGrupo");
-        for (Grupal opcion : selecciones){
-            mapaDeOpciones.put(opcion.texto(),opciones[i]);
-            opciones[i].setText(opcion.texto());
+        mapaDeGrupos.put("sinGrupo", boxGrupos.get(j++));
+        boxGrupos.get(j).setId("sinGrupo");
+        for (Grupal opcion : opciones){
+            labelsOpciones.get(i).setText(opcion.texto());
             if(!mapaDeGrupos.containsKey(opcion.grupo())){
-                mapaDeGrupos.put(opcion.grupo(), boxGrupos[j]);
-                grupos[j-1].setText(opcion.grupo());
-                boxGrupos[j].setId(opcion.grupo());
+                mapaDeGrupos.put(opcion.grupo(), boxGrupos.get(j));
+                labelsGrupos.get(j-1).setText(opcion.grupo());
+                boxGrupos.get(j).setId(opcion.grupo());
                 j++;
             }
             i++;
         }
-        BoxSinGrupo.getChildren().remove(i,opciones.length);
+        BoxSinGrupo.getChildren().remove(i,labelsOpciones.size());
         iniciarTemporizador();
     }
 
     private void actualizarVistaDeListas(){
-        for (Grupal opcion : selecciones){
-            if (!mapaDeGrupos.get(opcion.grupoSeleccionado()).getChildren().contains(mapaDeOpciones.get(opcion.texto()))){
-                mapaDeGrupos.get(opcion.grupoSeleccionado()).getChildren().add(mapaDeOpciones.get(opcion.texto()));
+        for (Grupal opcion : opciones){
+            if (!mapaDeGrupos.get(opcion.grupoSeleccionado()).getChildren().contains(labelsOpciones.get(opciones.indexOf(opcion)))){
+                mapaDeGrupos.get(opcion.grupoSeleccionado()).getChildren().add(labelsOpciones.get(opciones.indexOf(opcion)));
             }
         }
     }
@@ -66,7 +72,6 @@ public class ControladorGroupChoice extends ControladorPregunta{
         ClipboardContent content = new ClipboardContent();
         content.putString(((Label)mouseEvent.getSource()).getText());
         db.setContent(content);
-
         mouseEvent.consume();
     }
 
@@ -75,7 +80,7 @@ public class ControladorGroupChoice extends ControladorPregunta{
         boolean success = false;
         if (db.hasString()) {
             success = true;
-            for (Grupal opcion : selecciones) {
+            for (Grupal opcion : opciones) {
                 if (opcion.texto().equals(dragEvent.getDragboard().getString())) { //El else-if busca si es que uno esta dropeando sobre el objetivo o uno de sus hijos
                     if (dragEvent.getTarget().getClass() == VBox.class) opcion.seleccionar(((VBox) (dragEvent.getTarget())).getId());
                     else if (dragEvent.getTarget().getClass() == Label.class) opcion.seleccionar(((VBox)(((Label)dragEvent.getTarget()).getParent())).getId());
@@ -92,7 +97,6 @@ public class ControladorGroupChoice extends ControladorPregunta{
         if (dragEvent.getGestureSource().getClass() == Label.class && dragEvent.getDragboard().hasString()) {
             dragEvent.acceptTransferModes(TransferMode.COPY_OR_MOVE);
         }
-
         dragEvent.consume();
     }
 
@@ -106,10 +110,8 @@ public class ControladorGroupChoice extends ControladorPregunta{
 
     public void responder(ActionEvent actionEvent) throws IOException {
         accionExclusividad();
-        constructorDeRespuestaActual.conResponsable(jugadores.get(jugadorActual));
-        constructorDeRespuestaActual.conSelecciones(List.copyOf(selecciones));
+        constructorDeRespuestaActual.conSelecciones(List.copyOf(opciones));
         respuestas.add(constructorDeRespuestaActual.build());
         continuar();
     }
-
 }
