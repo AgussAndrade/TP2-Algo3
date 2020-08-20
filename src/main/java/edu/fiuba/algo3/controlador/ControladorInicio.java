0f3995c441f252d74dac910eeaee4bee877fadda
Dimartino <pdimartino@fi.uba.ar>
2020-08-20 14:48:02 -0300

    Se mejora el display de las opciones iniciales en MenuInicio

--- src/main/java/edu/fiuba/algo3/controlador/ControladorInicio.java
+++ src/main/java/edu/fiuba/algo3/controlador/ControladorInicio.java
@@ -4,11 +4,14 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.SplitMenuButton;
 
 import java.io.IOException;
 public class ControladorInicio extends ControladorPrincipal {
 
     public Button botonIniciar;
+    public SplitMenuButton menuMusica;
+    public SplitMenuButton menuTiempo;
 
     public void initialize(){
         Platform.runLater(()->botonIniciar.requestFocus());
@@ -21,26 +24,32 @@ public class ControladorInicio extends ControladorPrincipal {
     }
 
     public void pausarMusica(ActionEvent actionEvent) {
+        menuMusica.setText("Sin musica");
         flujoDePrograma.pausarMusica();
     }
 
     public void PonerKakut(ActionEvent actionEvent) {
+        menuMusica.setText("Kakut");
         flujoDePrograma.musica("kakut");
     }
 
     public void PonerClasico(ActionEvent actionEvent) {
+        menuMusica.setText("Clasico");
         flujoDePrograma.musica("clasico");
     }
 
     public void setearDiezSegundos(ActionEvent actionEvent) {
+        menuTiempo.setText("10");
         tiempoBase = 10;
     }
 
     public void setearVeinteSegundos(ActionEvent actionEvent) {
+        menuTiempo.setText("20");
         tiempoBase = 20;
     }
 
     public void setearTreintaSegundos(ActionEvent actionEvent) {
+        menuTiempo.setText("30");
         tiempoBase = 30;
     }
 }
